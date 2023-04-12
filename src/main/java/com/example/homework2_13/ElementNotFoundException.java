cb65a9f3f62ed6a25047dbd9f78794300efedf10
Anastasia <sychkova.nastyusha@mail.ru>
2023-04-12 13:25:07 +0400

    hw

---
+++ src/main/java/com/example/homework2_13/ElementNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.homework2_13;
+
+public class ElementNotFoundException extends RuntimeException {
+    public ElementNotFoundException(String massage) {
+        super(massage);
+    }
+}
